255cddba009c5c0bfbb982d7e6efe62af6a753a5
“Anastasia <anastasia.andreasen@gmail.com>
2021-07-05 19:05:53 +0200

    server changes

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require('express')
+const fs = require('fs')
+
 const app = express()
 
 app.use(express.static('public'))
 app.use(express.json())
 
-const fs = require('fs')
 const mainPage = fs.readFileSync(__dirname + '/public/index.html', 'utf-8')
 
 app.get('/', res => {
 	res.send(mainPage)
 })
 
-const server = app.listen(process.env.PORT || 8080, error => {
+const server = app.listen(process.env.PORT || 3000, error => {
 	if (error) {
 		console.log(error)
 	}
